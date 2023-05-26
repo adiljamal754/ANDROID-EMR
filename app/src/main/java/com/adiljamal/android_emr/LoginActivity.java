b4228a6f5176237f347f5184018faf808e3719de
Adil Jamal <adilabudine@gmail.com>
2023-05-27 01:35:39 +0200

    feature: adding home screen

--- app/src/main/java/com/adiljamal/android_emr/LoginActivity.java
+++ app/src/main/java/com/adiljamal/android_emr/LoginActivity.java
@@ -3,15 +3,23 @@ package com.adiljamal.android_emr;
 import androidx.appcompat.app.AppCompatActivity;
 import com.adiljamal.android_emr.databinding.ActivityLoginBinding;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //private ActivityLoginBinding loginBinding;
     private ActivityLoginBinding loginBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(loginBinding.getRoot());
+
+
+        loginBinding.loginBtn.setOnClickListener(v -> {
+            Intent navIt = new Intent(this, DrawerActivity.class);
+            startActivity(navIt);
+        });
     }
 }
--- app/src/main/java/com/adiljamal/android_emr/ui/home/HomeFragment.java
+++ app/src/main/java/com/adiljamal/android_emr/ui/home/HomeFragment.java
@@ -23,10 +23,9 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
     }
 
     @Override
